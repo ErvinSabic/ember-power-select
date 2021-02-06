3c75a23a8b84c58c0e76e9acca12cdd65fe93cdf
Ervin Sabic <esabic@gearsite.net>
2021-02-06 15:07:42 -0600

    made docs slightly better

--- tests/dummy/app/templates/snippets/component-object-passage-js.js
+++ tests/dummy/app/templates/snippets/component-object-passage-js.js
@@ -1,5 +1,12 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
 import { action } from '@ember/object';
 
-export default class extends Controller {
+export default class PassingComponents extends Controller {
+    cities = ['Barcelona', 'London', 'New York', 'Porto', 'Coruña', 'Kracow', 'Siena', 'Portland', 'Springfield', 'Tokio']
+    destination = 'London'
+
+    @action
+    chooseDestination(city) {
+        this.set('destination', city);
+    }
 }
